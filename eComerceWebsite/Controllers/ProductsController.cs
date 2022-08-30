7d6d6ceaf0ddcc1a740286fbd2f05333d7d9520d
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-08-30 01:00:31 -0700

    CreateProduct view fully functional and and added some styling.

--- eComerceWebsite/Controllers/ProductsController.cs
+++ eComerceWebsite/Controllers/ProductsController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eComerceWebsite.Data;
+using eComerceWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eComerceWebsite.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductsContext _context; // Must be read only.
+        public ProductsController(ProductsContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Products addedProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(addedProduct); // Prepares insert
+
+                _context.SaveChanges(); // Executes pending insert
+
+
+                ViewData["Message"] = $"{addedProduct.ProductName} was added successfully!";
+                return View();
+            }
+
+            return View(addedProduct);
         }
     }
 }
