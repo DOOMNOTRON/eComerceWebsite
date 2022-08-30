9369f36cdf1a530ab02c2bdaa3c1b72e67c9e9e5
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-08-30 01:16:53 -0700

    Refactored add product code to be asynchronous.

--- eComerceWebsite/Controllers/ProductsController.cs
+++ eComerceWebsite/Controllers/ProductsController.cs
@@ -19,13 +19,15 @@ namespace eComerceWebsite.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Products addedProduct)
+        public async Task<IActionResult> Create(Products addedProduct)
         {
             if (ModelState.IsValid)
             {
                 _context.Products.Add(addedProduct); // Prepares insert
 
-                _context.SaveChanges(); // Executes pending insert
+                // For async code info in the tutorial
+                // https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-6.0
+                await _context.SaveChangesAsync();  // Executes pending insert
 
 
                 ViewData["Message"] = $"{addedProduct.ProductName} was added successfully!";
