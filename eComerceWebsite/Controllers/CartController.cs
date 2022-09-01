b036c4dcb7150cdafbbd37c7a6b3249d319c3a94
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-09-01 07:54:34 -0700

    Coded add to cart, remove from cart, and added a cart summary page.

--- eComerceWebsite/Controllers/CartController.cs
+++ eComerceWebsite/Controllers/CartController.cs
@@ -1,12 +1,14 @@
 ﻿using eComerceWebsite.Data;
 using Microsoft.AspNetCore.Mvc;
 using eComerceWebsite.Models;
+using Newtonsoft.Json;
 
 namespace eComerceWebsite.Controllers
 {
     public class CartController : Controller
     {
         private readonly ProductsContext _context;
+        private const string Cart = "ShoppingCart";
 
         public CartController(ProductsContext context)
         {
@@ -17,16 +19,75 @@ namespace eComerceWebsite.Controllers
         {
             Products? productToAdd = _context.Products.Where(p => p.ProductID == id).SingleOrDefault();
 
-            if(productToAdd == null)
+            if (productToAdd == null)
             {
                 // Product with specified id does not exist
                 TempData["Message"] = "Sorry, that product no longer exists.";
                 return RedirectToAction("index", "Products");
             }
 
-            // 
+            CartProductViewModel cartProduct = new()
+            {
+                ProductID = productToAdd.ProductID,
+                ProductName = productToAdd.ProductName,
+                ProductPrice = productToAdd.ProductPrice,
+            };
+
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+            cartProducts.Add(cartProduct);
+            WriteShoppingCartCookie(cartProducts);
+
             TempData["Message"] = "Item added to cart!";
             return RedirectToAction("index", "Products");
         }
+
+        private void WriteShoppingCartCookie(List<CartProductViewModel> cartProducts)
+        {
+            string cookieData = JsonConvert.SerializeObject(cartProducts);
+
+            HttpContext.Response.Cookies.Append(Cart, cookieData, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddYears(1)
+            });
+        }
+
+        /// <summary>
+        /// Ruturn the current list of products in the users shopping
+        /// car cookie. If there is no cookie, an empty list will be returned.
+        /// </summary>
+        /// <returns></returns>
+        private List<CartProductViewModel> GetExistingCartData()
+        {
+            string? cookie = HttpContext.Request.Cookies[Cart];
+
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                return new List<CartProductViewModel>();
+            }
+
+            return JsonConvert.DeserializeObject<List<CartProductViewModel>>(cookie);
+        }
+
+        public  IActionResult Summary()
+        {
+            // Read shopping cart data and convert to list of view model
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+            return View(cartProducts);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartProductViewModel> cartProducts = GetExistingCartData();
+
+            CartProductViewModel targetProduct =
+                cartProducts.Where(p => p.ProductID == id).FirstOrDefault();
+
+            cartProducts.Remove(targetProduct);
+
+            WriteShoppingCartCookie(cartProducts);
+
+            return RedirectToAction(nameof(Summary));
+        }
+
     }
 }
