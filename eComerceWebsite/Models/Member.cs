9e05bb7713fae5f142b0777a091c37d351849441
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-09-01 04:14:49 -0700

    Added a view model for the Login page

--- eComerceWebsite/Models/Member.cs
+++ eComerceWebsite/Models/Member.cs
@@ -43,4 +43,14 @@ namespace eComerceWebsite.Models
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
 }
