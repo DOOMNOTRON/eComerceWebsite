667107a2c2a722040d926128046625eae412cf70
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-09-01 00:37:56 -0700

    Added migration for member class.

--- eComerceWebsite/Migrations/20220901073615_AddedMember.Designer.cs
+++ eComerceWebsite/Migrations/20220901073615_AddedMember.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eComerceWebsite.Data;
 
@@ -10,9 +11,10 @@ using eComerceWebsite.Data;
 namespace eComerceWebsite.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20220901073615_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
