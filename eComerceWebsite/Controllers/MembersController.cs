544ab2756debfc89ca07c41389a7cd508002b59f
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-09-01 01:36:29 -0700

    Added Register view

---
+++ eComerceWebsite/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eComerceWebsite.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- eComerceWebsite/Models/Member.cs
+++ eComerceWebsite/Models/Member.cs
@@ -33,12 +33,14 @@ namespace eComerceWebsite.Models
 
         [Required]
         [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
 
         [Required]
         [Compare(nameof(Password))]
         [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
